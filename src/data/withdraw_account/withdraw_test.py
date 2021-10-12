768632ba883f7bc2f5ef710650db6724e4a3e66c
Leonardo Verhagen Kava <leonardokava@hotmail.com>
2021-10-12 20:34:34 -0300

    Removing unnecessary code.

--- src/data/withdraw_account/withdraw_test.py
+++ src/data/withdraw_account/withdraw_test.py
@@ -6,34 +6,6 @@ from .withdraw import WithdrawAccount
 faker = Faker()
 
 
-# def test_withdraw_new_account():
-#     """Test for new account withdraw method"""
-
-#     account_repository = AccountRepositoryMock()
-#     get_account = GetAccountMock(account_repository)
-#     withdraw_account = WithdrawAccount(account_repository, get_account)
-
-#     attributes = {
-#         "account_id": faker.random_number(digits=3),
-#         "amount": faker.pyfloat(positive=True),
-#     }
-
-#     response = withdraw_account.withdraw(
-#         account_id=attributes["account_id"], amount=attributes["amount"]
-#     )
-
-#     assert (
-#         account_repository.create_account_params["account_id"]
-#         == attributes["account_id"]
-#     )
-#     assert account_repository.create_account_params["balance"] == attributes["amount"]
-
-#     assert get_account.account_id_param["account_id"] == attributes["account_id"]
-
-#     assert response["Success"] is True
-#     assert response["Data"]
-
-
 def test_withdraw_existing_account():
     """Test for existing account withdraw method"""
 
