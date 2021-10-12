d0b6dc2158ab505ba6475f494d32c0924578b7bd
Leonardo Verhagen Kava <leonardokava@hotmail.com>
2021-10-12 10:32:48 -0300

    Implementing domain layer, with accounts model.

---
+++ src/domain/models/accounts.py
@@ -0,0 +1,3 @@
+from collections import namedtuple
+
+Accounts = namedtuple("Accounts", "id balance")
--- src/infra/repo/account_repository.py
+++ src/infra/repo/account_repository.py
@@ -1,6 +1,7 @@
 from collections import namedtuple
+from src.domain.models import Accounts
 from src.infra.config import DBConnectionHandler
-from src.infra.entities import Accounts
+from src.infra.entities import Accounts as AccountsModel
 
 
 class AccountRepository:
@@ -17,7 +18,7 @@ class AccountRepository:
 
         with DBConnectionHandler() as db_connection:
             try:
-                new_account = Accounts(id=account_id, balance=balance)
+                new_account = AccountsModel(id=account_id, balance=balance)
                 db_connection.session.add(new_account)
                 db_connection.session.commit()
 
