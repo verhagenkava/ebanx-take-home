9b425e4a9ec19e49143bc2d27d59c1a7b0374b9c
Leonardo Verhagen Kava <leonardokava@hotmail.com>
2021-10-13 12:51:18 -0300

    Implement HTTP helpers.

---
+++ src/presenters/helpers/http_models.py
@@ -0,0 +1,26 @@
+from typing import Dict
+
+
+class HttpRequest:
+    """HTTP Requests representation class"""
+
+    def __init__(self, header: Dict = None, body: Dict = None, query: Dict = None):
+        self.header = header
+        self.body = body
+        self.query = query
+
+    def __repr__(self):
+        return (
+            f"HttpRequest (header={self.header}, body={self.body}, query={self.query})"
+        )
+
+
+class HttpResponse:
+    """HTTP Responses representation class"""
+
+    def __init__(self, status_code: int, body: any):
+        self.status_code = status_code
+        self.body = body
+
+    def __repr__(self):
+        return f"HttpResponse (status_code={self.status_code}, body={self.body})"
